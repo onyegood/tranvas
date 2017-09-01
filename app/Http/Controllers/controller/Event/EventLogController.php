70b58ab7c0c3008d09ec4f5339fb9ba2de683fc1
Onyeka Ofoegbu <onyeka.ofoegbu@workforcegroup.com>
2017-09-01 14:03:49 +0100

    Added Event log model and controller

---
+++ app/Http/Controllers/controller/Event/EventLogController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\controller\Event;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Event\EventLog;
+
+class EventLogController extends Controller
+{
+    //
+}
---
+++ app/Models/Event/EventLog.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Event;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventLog extends Model
+{
+    protected $guarded = [];
+}
